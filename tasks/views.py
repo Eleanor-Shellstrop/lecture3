1fb62d7566fc13751344cb67bacc38a98c81e774
Anne Ensign <anne.ensign@gmail.com>
2021-08-26 11:14:42 -0400

    Add sessions variable

--- tasks/views.py
+++ tasks/views.py
@@ -3,17 +3,21 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-tasks = []
-
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     # Could add other fields, such as:
     # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
+#TODO: Find why index(request) isn't enough to carry this variable (line 28 error)
+tasks = []
+
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
@@ -21,7 +25,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [tasks]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
